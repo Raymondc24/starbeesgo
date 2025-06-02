0ae4aa47e30972fda1279db1f35a994258fcc26f
RAYMOND CHIENG CHING BING <166306704+Raymondc24@users.noreply.github.com>
2025-06-03 00:13:56 +0800

    Delete Setup (Edited) directory

--- Setup (Edited)/Admin/Admin_Main_Script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("userInfoButton").onclick = () => {
-    window.location.href = "Admin_User_Index.html";
-};
-document.getElementById("paymentButton").onclick = () => {
-    window.location.href = "Admin_Payment_Index.html";
-};
