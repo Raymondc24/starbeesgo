19c49d41afa65db70c08a0734ef1f34f1caa29e9
Skye Chase <172270210+SkyEmperer@users.noreply.github.com>
2025-06-02 21:37:49 +0800

    Add files via upload

---
+++ Setup (Edited)/Admin/Admin_Main_Script.js
@@ -0,0 +1,6 @@
+document.getElementById("userInfoButton").onclick = () => {
+    window.location.href = "Admin_User_Index.html";
+};
+document.getElementById("paymentButton").onclick = () => {
+    window.location.href = "Admin_Payment_Index.html";
+};
